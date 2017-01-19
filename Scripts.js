a0f1f0eda5d737279efdd782fc6046b4c5cac2fd
Katulobotomia-läbäri <joonas.natynki@gmail.com>
2017-01-19 13:41:49 +0200

    Working map box

---
+++ Scripts.js
@@ -0,0 +1,12 @@
+function initMap() 
+{
+        var uluru = {lat: 60.220859, lng: 24.805229};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
